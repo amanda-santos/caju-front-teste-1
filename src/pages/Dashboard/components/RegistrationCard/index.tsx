4af47ef1588bbd332c24d3b4539b2a094294e314
Amanda Santos <amandasf0200@gmail.com>
2024-08-17 19:49:01 -0300

    feat: show different buttons depending on registration status

--- src/pages/Dashboard/components/RegistrationCard/index.tsx
+++ src/pages/Dashboard/components/RegistrationCard/index.tsx
@@ -6,35 +6,49 @@ import {
   HiOutlineCalendar,
   HiOutlineTrash,
 } from "react-icons/hi";
+import { Registration, RegistrationStatus } from "~/types/Registration";
 
-type Props = {
-  data: any;
+type RegistrationCardProps = {
+  data: Pick<
+    Registration,
+    "employeeName" | "email" | "admissionDate" | "status"
+  >;
 };
 
-const RegistrationCard = (props: Props) => {
+export const RegistrationCard = ({
+  data: { admissionDate, email, employeeName, status },
+}: RegistrationCardProps) => {
+  const isRegistrationReviewed =
+    status === RegistrationStatus.Approved ||
+    status === RegistrationStatus.Reproved;
+  const isRegistrationPending = status === RegistrationStatus.Review;
+
   return (
     <S.Card>
       <S.IconAndText>
         <HiOutlineUser />
-        <h3>{props.data.employeeName}</h3>
+        <h3>{employeeName}</h3>
       </S.IconAndText>
       <S.IconAndText>
         <HiOutlineMail />
-        <p>{props.data.email}</p>
+        <p>{email}</p>
       </S.IconAndText>
       <S.IconAndText>
         <HiOutlineCalendar />
-        <span>{props.data.admissionDate}</span>
+        <span>{admissionDate}</span>
       </S.IconAndText>
       <S.Actions>
-        <ButtonSmall bgcolor="rgb(255, 145, 154)" >Reprovar</ButtonSmall>
-        <ButtonSmall bgcolor="rgb(155, 229, 155)">Aprovar</ButtonSmall>
-        <ButtonSmall bgcolor="#ff8858">Revisar novamente</ButtonSmall>
-
+        {isRegistrationPending && (
+          <>
+            <ButtonSmall bgcolor="rgb(255, 145, 154)">Reprovar</ButtonSmall>
+            <ButtonSmall bgcolor="rgb(155, 229, 155)">Aprovar</ButtonSmall>
+          </>
+        )}
+        {isRegistrationReviewed && (
+          <ButtonSmall bgcolor="#ff8858">Revisar novamente</ButtonSmall>
+        )}
         <HiOutlineTrash />
       </S.Actions>
     </S.Card>
   );
 };
-
-export default RegistrationCard;
