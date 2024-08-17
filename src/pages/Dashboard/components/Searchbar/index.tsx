22607cccdcccf2e86b0a4f9caae3e8b97c39f07c
Amanda Santos <amandasf0200@gmail.com>
2024-08-17 20:20:28 -0300

    feat: implement refetch action

--- src/pages/Dashboard/components/Searchbar/index.tsx
+++ src/pages/Dashboard/components/Searchbar/index.tsx
@@ -5,18 +5,27 @@ import { IconButton } from "~/components/Buttons/IconButton";
 import TextField from "~/components/TextField";
 import routes from "~/router/routes";
 import * as S from "./styles";
+import { useQueryClient } from "@tanstack/react-query";
+
 export const SearchBar = () => {
   const history = useHistory();
+  const queryClient = useQueryClient();
+
+  const handleRefetch = () => {
+    queryClient.refetchQueries({
+      queryKey: ["registrations"],
+    });
+  };
 
   const goToNewAdmissionPage = () => {
     history.push(routes.newUser);
   };
-  
+
   return (
     <S.Container>
-      <TextField  placeholder="Digite um CPF válido" />
+      <TextField placeholder="Digite um CPF válido" />
       <S.Actions>
-        <IconButton aria-label="refetch">
+        <IconButton aria-label="refetch" onClick={handleRefetch}>
           <HiRefresh />
         </IconButton>
         <Button onClick={() => goToNewAdmissionPage()}>Nova Admissão</Button>
